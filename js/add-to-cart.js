0df625665bb8b333194ae18659f6ef59c1c9fd0b
Brenden Mersey <brenden.mersey@weareverypolite.com>
2022-05-06 13:45:53 -0700

    Addition of eventlistener for the js--add-to-cart class.

--- js/add-to-cart.js
+++ js/add-to-cart.js
@@ -1,23 +1,36 @@
-let formData = {
- 'items': [
-    {
-      'id': 36110175633573,
-      'quantity': 1
-    }
-  ]
-};
+// let formData = {
+//  'items': [
+//     {
+//       'id': 36110175633573,
+//       'quantity': 1
+//     }
+//   ]
+// };
 
-fetch(window.Shopify.routes.root + 'cart/add.js', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify(formData)
-})
-.then(response => {
-  return response.json();
-})
-.catch((error) => {
-  console.error('Error:', error);
+// fetch(window.Shopify.routes.root + 'cart/add.js', {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json'
+//   },
+//   body: JSON.stringify(formData)
+// })
+// .then(response => {
+//   return response.json();
+// })
+// .catch((error) => {
+//   console.error('Error:', error);
+// });
+
+( document.querySelectorAll('.js--add-to-cart') || [] ).forEach( button => {
+  trigger.addEventListener('click', event => {
+
+    event.preventDefault();
+
+    let variantID = button.dataset.variantId || false;
+    let quantity = button.dataset.quantity || 1;
+
+    console.lgo({ variantID, quantity });
+
+  });
 });
 
