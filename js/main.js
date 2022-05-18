3264ca83e03867f1f9dbff3158e925cb83c1c9be
Brenden Mersey <brenden.mersey@weareverypolite.com>
2022-05-17 23:30:10 -0700

    Working through the recommend products feature.

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 // @codekit-prepend quiet "../node_modules/@glidejs/glide/dist/glide.min.js";
 // @codekit-prepend quiet "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 // @codekit-prepend quiet "../node_modules/swiper/swiper-bundle.min.js";
+// @codekit-prepend quiet "../node_modules/@shopify/theme-images/dist/images.es5.js";
 
 // @codekit-prepend "./modules/_credits.js";
 // @codekit-prepend "./modules/_breakpoints.js";
@@ -87,3 +88,45 @@ var swiper = new Swiper(".swiper-main", {
     swiper: swiperThumbs
   }
 });
+
+
+
+
+
+//   var loadProductRecommendationsIntoSection = function() {
+//     // Look for an element with class 'product-recommendations'
+//     var productRecommendationsSection = document.querySelector(".product-recommendations");
+//     if (productRecommendationsSection === null) { return; }
+//     // Read product id from data attribute
+//     var productId = productRecommendationsSection.dataset.productId;
+//     // Read limit from data attribute
+//     var limit = productRecommendationsSection.dataset.limit;
+//     // Build request URL
+//     // var requestUrl = "/recommendations/products?section_id=product-recommendations&limit="+limit+"&product_id="+productId;
+//
+//     var requestUrl = productRecommendationsSection.dataset.url || false;
+//
+//     // Create request and submit it using Ajax
+//     var request = new XMLHttpRequest();
+//     request.open("GET", requestUrl);
+//     request.onload = function() {
+//       if (request.status >= 200 && request.status < 300) {
+//         var container = document.createElement("div");
+//         container.innerHTML = request.response;
+//         productRecommendationsSection.parentElement.innerHTML = container.querySelector(".product-recommendations").innerHTML;
+//       }
+//     };
+//     request.send();
+//     console.log('loadProductRecommendationsIntoSection()');
+//   };
+//   // If your section has theme settings, the theme editor
+//   // reloads the section as you edit those settings. When that happens, the
+//   // recommendations need to be fetched again.
+//   // See https://help.shopify.com/en/themes/development/sections/integration-with-theme-editor
+//   document.addEventListener("shopify:section:load", function(event) {
+//     if (event.detail.sectionId === "product-recommendations") {
+//       loadProductRecommendationsIntoSection();
+//     }
+//   });
+//   // Fetching the recommendations on page load
+//   loadProductRecommendationsIntoSection();
