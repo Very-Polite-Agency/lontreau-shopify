4bfda91d8f5ca70aef9ac2268617930d88201185
Brenden Mersey <brenden.mersey@weareverypolite.com>
2022-05-24 09:19:07 -0700

    QC

--- js/main.js
+++ js/main.js
@@ -52,6 +52,7 @@ AOS.init({
   delay: 0,                   // values from 0 to 3000, with step 50ms
   duration: 550,              // values from 0 to 3000, with step 50ms
   easing: 'ease-in-out',      // default easing for AOS animations
+  once: true,
 });
 
 var swiperThumbs = new Swiper(".swiper-thumbs", {
